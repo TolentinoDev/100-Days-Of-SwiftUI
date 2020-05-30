21002344dab29b458ecc32624bc143576a3297ff
Ryan Tolentino <Tolentino2016@gmail.com>
2020-05-30 16:44:30 -0500

    Day-10 Complete

---
+++ Day-10/day10.playground/Contents.swift
@@ -0,0 +1,196 @@
+class Image {
+    var filename: String
+    var isAnimated: Bool
+    init(filename: String, isAnimated: Bool) {
+        self.filename = filename
+        self.isAnimated = isAnimated
+    }
+}
+
+class ThemePark {
+    var entryPrice: Int
+    var rides: [String]
+    init(rides: [String]) {
+        self.rides = rides
+        self.entryPrice = rides.count * 2
+    }
+}
+
+
+class Empty { }
+let nothing = Empty()
+
+
+class Podcast {
+    var hosts: [String]
+    init(hosts: [String]) {
+        self.hosts = hosts
+    }
+}
+
+class Vehicle {
+    var wheels: Int
+    init(wheels: Int) {
+        self.wheels = wheels
+    }
+}
+
+
+
+class Handbag {
+    var price: Int
+    init(price: Int) {
+        self.price = price
+    }
+}
+class DesignerHandbag: Handbag {
+    var brand: String
+    init(brand: String, price: Int) {
+        self.brand = brand
+        super.init(price: price)
+    }
+}
+
+
+class SmartPhone {
+    var price: Int
+    init(price: Int) {
+        self.price = price
+    }
+}
+
+
+class Printer {
+    var cost: Int
+    init(cost: Int) {
+        self.cost = cost
+    }
+}
+class LaserPrinter: Printer {
+    var model: String
+    init(model: String, cost: Int) {
+        self.model = model
+        super.init(cost: cost)
+    }
+}
+
+
+class Shape {
+    var sides: Int
+    init(sides: Int) {
+        self.sides = sides
+    }
+}
+class Rectangle: Shape {
+    var color: String
+    init(color: String) {
+        self.color = color
+        super.init(sides: 4)
+    }
+}
+
+
+class Appliance {
+    func start() {
+        print("Starting...")
+    }
+}
+class Oven: Appliance {
+}
+let oven = Oven()
+oven.start()
+
+
+class Exercise {
+    func describe() {
+    }
+}
+class ChinUps: Exercise {
+    override func describe() {
+    }
+}
+let firstRep = ChinUps()
+firstRep.describe()
+
+
+class Statue {
+    var sculptor = "Unknown"
+}
+var venusDeMilo = Statue()
+venusDeMilo.sculptor = "Alexandros of Antioch"
+var david = Statue()
+david.sculptor = "Michaelangelo"
+print(venusDeMilo.sculptor)
+print(david.sculptor)
+
+
+class Starship {
+    var maxWarp = 9.0
+}
+var voyager = Starship()
+voyager.maxWarp = 9.975
+var enterprise = voyager
+enterprise.maxWarp = 9.6
+print(voyager.maxWarp)
+print(enterprise.maxWarp)
+
+
+struct Calculator {
+    var currentTotal = 0
+}
+var baseModel = Calculator()
+var casio = baseModel
+var texas = baseModel
+casio.currentTotal = 556
+texas.currentTotal = 384
+print(casio.currentTotal)
+print(texas.currentTotal)
+
+
+class Hater {
+    var isHating = true
+}
+var hater1 = Hater()
+var hater2 = hater1
+hater1.isHating = false
+print(hater1.isHating)
+print(hater2.isHating)
+
+
+class Magazine {
+    var pageCount = 132
+}
+var example = Magazine()
+var wired = example
+wired.pageCount = 164
+var vogue = example
+vogue.pageCount = 128
+print(wired.pageCount)
+print(vogue.pageCount)
+
+class Election {
+    init() {
+        print("And the winner is...")
+    }
+}
+
+
+class Pizza {
+    private var toppings = [String]()
+    func add(topping: String) {
+        toppings.append(topping)
+    }
+}
+var pizza = Pizza()
+pizza.add(topping: "Mushrooms")
+
+struct Park {
+    var numberOfFlowers = 1000
+    mutating func plantFlowers() {
+        numberOfFlowers += 50
+    }
+}
+
+
+var park = Park()
+park.plantFlowers()
