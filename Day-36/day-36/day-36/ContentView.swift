4f9e904744436082d9583aa0dcc8b1316afcdd91
Ryan Tolentino <Tolentino2016@gmail.com>
2020-06-29 15:32:40 -0500

    Day-36 Complete

--- Day-36/day-36/day-36/ContentView.swift
+++ Day-36/day-36/day-36/ContentView.swift
@@ -8,21 +8,63 @@
 
 import SwiftUI
 
-struct User {
-    var firstName = "Bilbo"
-    var lastName = "Baggins"
+struct SecondView: View {
+    @Environment(\.presentationMode) var presentationMode
+    var name: String
+    var body: some View{
+        VStack{
+        Text("Hello \(name)!")
+        Button("Dismiss"){
+            self.presentationMode.wrappedValue.dismiss()
+        }
+    }
+    }
+    
+}
+
+class User: ObservableObject {
+    @Published var firstName = "Bilbo"
+    @Published var lastName = "Baggins"
 }
 
 
 struct ContentView: View {
-    @State private var user = User()
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+    @State private var showingSheet = false
+    @ObservedObject private var user = User()
     
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName).")
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
+        NavigationView{
+            VStack {
+                List{
+                    ForEach(numbers, id: \.self) {
+                        Text("\($0)")
+                    }
+                .onDelete(perform: removeRows)
+                }
+                Button("Add Numbers: \(tapCount)") {
+                    self.numbers.append(self.currentNumber)
+                    self.currentNumber += 1
+                    self.tapCount += 1
+                    UserDefaults.standard.set(self.tapCount, forKey: "Tap")
+                }
+                Text("Your name is \(user.firstName) \(user.lastName).")
+                TextField("First name", text: $user.firstName)
+                TextField("Last name", text: $user.lastName)
+                Button("Show Sheet"){
+                    self.showingSheet.toggle()
+                }
+                .sheet(isPresented: $showingSheet){
+                    SecondView(name: "@TolentinoDev")
+                }
+            }
+            .navigationBarItems(leading: EditButton())
         }
+    }
+    func removeRows(at offsets: IndexSet){
+        numbers.remove(atOffsets:offsets)
     }
 }
 
